bf9f5b8542aced08e4ae348365d998110d550ded
nayy-dev7 <nayaa.adalah.saya@gmail.com>
2025-10-01 06:31:50 +0700

    prak 1 jobsheer 4 kelar

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,4 +11,5 @@ class UserModel extends Model
 
     protected $table = 'm_user'; //mendefinisikan nama tabel yang digunakan oleh model tabel ini
     protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel yang digunakan
+    protected $fillable = ['level_id', 'username', 'nama'];
 }
